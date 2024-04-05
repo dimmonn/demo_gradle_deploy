e7178825e87dca752e9c5262ba44d6d5b609310e
dpolishchuk <dpolishchuk@vmware.com>
2024-04-05 09:57:08 +0200

    adding tests

---
+++ src/main/java/com/uj/demo/demo/misc/Calculator.java
@@ -0,0 +1,7 @@
+package com.uj.demo.demo.misc;
+
+public class Calculator {
+    public int add(int i, int i1) {
+        return 0;
+    }
+}
--- src/test/java/com/uj/demo/demo/controllers/GymControllerTest.java
+++ src/test/java/com/uj/demo/demo/controllers/GymControllerTest.java
@@ -1,12 +1,18 @@
 package com.uj.demo.demo.controllers;
 
+import com.uj.demo.demo.misc.Calculator;
+import com.uj.demo.demo.repositories.CoachRepository;
+import com.uj.demo.demo.repositories.EventRepository;
+import com.uj.demo.demo.repositories.GymRepository;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static junit.framework.TestCase.assertEquals;
 import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.*;
 import static org.springframework.http.MediaType.APPLICATION_JSON;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
@@ -17,6 +23,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class GymControllerTest {
     @Autowired
     private MockMvc mockMvc;
+
 
     @Test
     void addGym() throws Exception {
@@ -31,5 +38,26 @@ class GymControllerTest {
                 .andExpect(status().isOk()).andExpect(content().string(
                         containsString("{}")
                 ));
+    }
+
+    @Test
+    public void testAddWithMock() {
+        Calculator calculatorMock = mock(Calculator.class);
+        when(calculatorMock.add(3, 7)).thenReturn(10);
+
+        int result = calculatorMock.add(3, 7);
+
+        assertEquals(10, result);
+    }
+    @Test
+    public void testAddWithSpy() {
+        Calculator calculator = new Calculator();
+        Calculator calculatorSpy = spy(calculator);
+        when(calculatorSpy.add(3, 7)).thenReturn(10);
+
+        int result = calculatorSpy.add(3, 7);
+
+        assertEquals(10, result);
+        verify(calculatorSpy).add(3, 7);
     }
 }
